785cbfb9f340a108f6887e658ff741890443f496
cuprehan <51897056+cuprehan@users.noreply.github.com>
2019-11-12 23:44:54 -0500

    Reformatting. Can not yet get Sprites to show up

--- game1_ada_or_potato.py
+++ game1_ada_or_potato.py
@@ -5,34 +5,29 @@ WINDOW_WIDTH = 500
 WINDOW_HEIGHT = 500
 BACKGROUND_COLOR = arcade.color.BLACK
 GAME_TITLE = "Ada or Potato"
-GAME_SPEED = 1/60
+GAME_SPEED = 1 / 60
 
 TIMER_MAXIMUM = 100
 
-ADA_IMAGE = arcade.load_texture("images/ada.png", scale=.5)
-POTATO_IMAGE = arcade.load_texture("images/potato.png", scale=.5)
+ADA_IMAGE = arcade.load_texture("images/ada.png")
+POTATO_IMAGE = arcade.load_texture("images/potato.png")
+
 
 class Ada_Or_Potato(arcade.Window):
-    sprites: arcade.SpriteList
-    score: int
-
     def __init__(self):
-        """ Initialize variables """
         super().__init__(WINDOW_WIDTH, WINDOW_HEIGHT, GAME_TITLE)
         self.logo_list = None
         self.score = 0
         self.timer = 0
 
     def setup(self):
-        """ Setup the game (or reset the game) """
         arcade.set_background_color(BACKGROUND_COLOR)
-        self.score = 0
         self.logo_list = arcade.SpriteList()
         self.logo_list.append(ada())
         self.logo_list.append(potato())
+        self.score = 0
 
     def on_draw(self):
-        """ Called when it is time to draw the world """
         arcade.start_render()
         self.logo_list.draw()
         score = self.score
@@ -40,9 +35,8 @@ class Ada_Or_Potato(arcade.Window):
         arcade.draw_text("Your score:" + score, 10, 20, arcade.color.WHITE, 14)
 
     def on_update(self, delta_time):
-        """ Called every frame of the game (1/GAME_SPEED times per second)"""
         self.logo_list.update()
-        self.timer = self.timer + (1/60)
+        self.timer = self.timer + (1 / 60)
 
     def on_mouse_press(self, x: float, y: float, button: int, modifiers: int):
         if 1 <= self.timer % 2 < 2:
@@ -51,29 +45,35 @@ class Ada_Or_Potato(arcade.Window):
             self.score = self.score - 1
 
 
-class ada(arcade.Window):
+class ada(arcade.Sprite):
     def __init__(self):
-        """ Initialize variables """
         super().__init__("images/ada.png")
         self.timer = 0
-        self.center_x = WINDOW_WIDTH/2
-        self.center_y = WINDOW_HEIGHT/2
+        self.center_x = WINDOW_WIDTH / 2
+        self.center_y = WINDOW_HEIGHT / 2
 
-    def on_update(self, delta_time):
-        """ Called every frame of the game (1/GAME_SPEED times per second)"""
-        self.timer = self.timer + (1/60)
+    def on_update(self):
+        self.timer = self.timer + (1 / 60)
         if 1 <= self.timer % 2 < 2:
             self.alpha = 255
         else:
             self.alpha = 0
 
-class potato(arcade.Window):
-    def __init__(self):
-        """ Initialize variables """
-        super().__init__(WINDOW_WIDTH, WINDOW_HEIGHT, GAME_TITLE)
 
-    def on_update(self, delta_time):
-        """ Called every frame of the game (1/GAME_SPEED times per second)"""
+class potato(arcade.Sprite):
+    def __init__(self):
+        super().__init__("images/potato.png", scale=.5)
+        self.timer = 0
+        self.center_x = WINDOW_WIDTH / 2
+        self.center_y = WINDOW_HEIGHT / 2
+
+    def on_update(self):
+        self.timer = self.timer + (1 / 60)
+        if 1 <= self.timer % 2 < 1:
+            self.alpha = 255
+        else:
+            self.alpha = 0
+
 
 def main():
     window = Ada_Or_Potato()
